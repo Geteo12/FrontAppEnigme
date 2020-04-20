90fd4e66ba19c5f9e0f72cf8173809a58835fb23
Batmanovich <gomesdesousa.kevin@gmail.com>
2020-04-20 14:09:44 +0200

    Changement Enigme (DEPRECATED)

--- src/app/enigme/enigme.component.ts
+++ src/app/enigme/enigme.component.ts
@@ -7,22 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EnigmeComponent implements OnInit {
 
-  public show:boolean = false;
-  public buttonName:any = 'Show';
+
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  appear(){
-    this.show = !this.show;
+  
 
-    // CHANGE THE NAME OF THE BUTTON.
-    if(this.show)  
-      this.buttonName = "Hide";
-    else
-      this.buttonName = "Show";
-  }
 
 }
